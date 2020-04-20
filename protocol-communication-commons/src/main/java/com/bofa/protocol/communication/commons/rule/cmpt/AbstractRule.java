7a32f860549246cdf38eb979e9398cef744f16a0
BOFA1ex <BOFA1ex@163.com>
2020-04-21 03:49:53 +0800

    action直接异步提交任务去执行, 不再设置回调, 然后在decoder执行.

--- protocol-communication-commons/src/main/java/com/bofa/protocol/communication/commons/rule/cmpt/AbstractRule.java
+++ protocol-communication-commons/src/main/java/com/bofa/protocol/communication/commons/rule/cmpt/AbstractRule.java
@@ -2,8 +2,16 @@ package com.bofa.protocol.communication.commons.rule.cmpt;
 
 
 import com.bofa.protocol.communication.commons.rule.constant.RuleConstants;
+import com.bofa.protocol.communication.commons.rule.model.RulePacket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import javax.annotation.Resource;
+import java.util.Optional;
+import java.util.function.*;
 
 /**
  * @author bofa1ex
@@ -11,11 +19,30 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public abstract class AbstractRule implements InitializingBean, RuleConstants {
 
+    protected static final Logger logger = LoggerFactory.getLogger(AbstractRule.class);
+
     @Autowired
     private RuleManager ruleManager;
 
+    @Resource(name = "businessExecutor")
+    protected ThreadPoolTaskExecutor executor;
+
     @Override
-    public /* 子类无需复写 */ final void afterPropertiesSet(){
+    public /* 子类无需复写 */ final void afterPropertiesSet() {
         ruleManager.register(this);
+    }
+
+    protected Function<Throwable, ? extends Void> processError(){
+        return t -> {
+            logger.error("解析失败: {}",t.getMessage());
+            return null;
+        };
+    }
+
+    protected Consumer<Void> writeAndFlush(RulePacket packet) {
+        return v -> {
+            // 下发dst指令(判空处理)
+            Optional.ofNullable(packet.getDstBuffer()).ifPresent(buffer -> packet.getChannel().writeAndFlush(buffer));
+        };
     }
 }
